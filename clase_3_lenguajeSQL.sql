e34d21416690e210e6fed8f5354626221ec5d785
Christian Bustamante <clb.cristian@gmail.com>
2024-09-22 22:28:43 -0300

    primer commit

---
+++ clase_3_lenguajeSQL.sql
@@ -0,0 +1,34 @@
+
+
+CREATE TABLE Empleado(
+    id_empleado INT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    salario DECIMAL(10, 2),
+    cargo VARCHAR(50),
+    id_departamento INT,
+    FOREIGN KEY id_departamento REFERENCES Departamento(id_departamento)
+);
+
+CREATE TABLE Departamento(
+    id_departamento INT PRIMARY KEY,
+    nombre_departamento VARCHAR(50) NOT NULL,
+    ubicacion VARCHAR(100),
+    id_jefe INT,
+    FOREIGN KEY(id_jefe) REFERENCES Empleado(id_empleado)
+);
+
+CREATE TABLE Proyecto (
+    id_proyecto INT PRIMARY KEY,
+    nombre_proyecto VARCHAR(100) NOT NULL,
+    fecha_inicio DATE,
+    fecha_fin DATE
+);
+
+CREATE TABLE Asignacion (
+    id_asignacion INT PRIMARY KEY,
+    id_empleado INT, 
+    id_proyecto INT,  
+    rol VARCHAR(50),
+    FOREIGN KEY (id_empleado) REFERENCES Empleado(id_empleado),
+    FOREIGN KEY (id_proyecto) REFERENCES Proyecto(id_proyecto)
+);
